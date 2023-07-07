464156c911eb43c36e44dac75f5a6792b7b60f7d
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-07 10:32:14 +0200

    Add parametrized test

---
+++ src/test/java/org/example/tests/CreateSpaceWithParamsTest.java
@@ -0,0 +1,42 @@
+package org.example.tests;
+
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.example.requests.space.CreateSpaceRequest;
+import org.example.requests.space.DeleteSpaceRequest;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class CreateSpaceWithParamsTest {
+
+    @ParameterizedTest(name = "Create space with space name: {0}")
+    @DisplayName("Create space with valid space name")
+    @MethodSource("createSpaceData")
+    void createSpaceTest(String spaceName){
+
+        JSONObject space = new JSONObject();
+        space.put("name", spaceName);
+
+        final Response response = CreateSpaceRequest.createSpace(space);
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(200);
+        Assertions.assertThat(response.jsonPath().getString("name")).isEqualTo(spaceName);
+
+        final Response deleteResponse = DeleteSpaceRequest.deleteSpace(response.jsonPath().getString("id"));
+        Assertions.assertThat(deleteResponse.statusCode()).isEqualTo(200);
+        Assertions.assertThat(deleteResponse.asString()).isEqualTo("{}");
+    }
+
+    private static Stream<Arguments> createSpaceData() {
+        return Stream.of(
+                Arguments.of("TEST SPACE"),
+                Arguments.of("123"),
+                Arguments.of("*")
+        );
+    }
+}

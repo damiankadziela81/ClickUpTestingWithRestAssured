c715af1ba8ea448cfe0eddd7d678c18b778dd92c
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-08 13:35:41 +0200

    Add system properties so we can run it on Jenkins

--- src/main/java/org/example/properties/ClickUpProperties.java
+++ src/main/java/org/example/properties/ClickUpProperties.java
@@ -8,11 +8,19 @@ public class ClickUpProperties {
     private static final String TEAM_ID = "clickup.teamid";
 
     public static String getToken() {
-        return getProperty(TOKEN);
+        if (getProperty(TOKEN).isEmpty() || getProperty(TOKEN).startsWith("YOUR")) {
+            return System.getProperty("TOKEN");
+        } else {
+            return getProperty(TOKEN);
+        }
     }
 
     public static String getTeamId() {
-        return getProperty(TEAM_ID);
+        if (getProperty(TEAM_ID).isEmpty() || getProperty(TEAM_ID).startsWith("YOUR")) {
+            return  System.getProperty("TEAM_ID");
+        } else {
+            return getProperty(TEAM_ID);
+        }
     }
 
     private static String getProperty(String key) {
